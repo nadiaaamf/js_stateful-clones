d37d4ef3e3d812cd5be057f8832c8eb7aea943d1
Nádia <nadia.mfaria@hotmail.com>
2025-06-13 16:47:40 -0300

    making a function for the exercise

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const stateInicial = [];
+  let currentState = { ...state };
 
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    } else if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    stateInicial.push({ ...currentState });
+  }
+
+  return stateInicial;
+}
 module.exports = transformStateWithClones;
